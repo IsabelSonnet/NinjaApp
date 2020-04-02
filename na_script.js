3b483b06167fc4b7984d223f3ab5f90a362b959b
Samanthacgorton <samantha@Samanthas-MacBook-Pro.local>
2020-04-02 16:00:43 -0700

    COP MOVES

--- na_script.js
+++ na_script.js
@@ -1,11 +1,24 @@
-$(document).ready(() => {
-
-    $("button").click(function () {
-        $("#cop").animate({
-
-        });
-        $("#robber").animate({
-
-        });
-    });
-});
+//$(document).ready(() => {
+//
+//    $("button").click(function () {
+//        $("#cop").animate({
+//
+//$(document).ready(() => {
+//
+//    $("button").click(function () {
+//        $("#cop").animate({
+//
+//        });
+//        $("#robber").animate({
+//
+//        });
+//    });
+//});
+$(document).keydown(function(event){
+     var keycode = (event.keyCode ? event.keyCode : event.which);
+                if(keycode == '39'){
+                    $("#cop").animate({
+                         left: '+=10px',
+                    });
+                }
+     });
