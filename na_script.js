4202a5730ed30542b448e6963475f6a73154afd6
IsabelSonnet <izzysonnet@gmail.com>
2020-04-02 16:02:20 -0700

    this is wrong

--- na_script.js
+++ na_script.js
@@ -1,11 +1,33 @@
 $(document).ready(() => {
-
-    $("button").click(function () {
-        $("#cop").animate({
-
-        });
-        $("#robber").animate({
-
-        });
-    });
+    var $startbutton = $("#start");
+    var $cop = $("#cop");
+    var $robber = $("#robber");
+    var $copimg = $("#copimg");
+  $startbutton.click(function () {
+    if (clicked == false) {
+      $copimg.animate({
+        width: "300px",
+        height: "300px",
+        marginLeft: "150px",
+        opacity: 0.5
+      });
+      clicked = true;
+    } else {
+      $cop.animate({
+        width: "100px",
+        height: "100px",
+        marginLeft: "0px",
+        borderWidth: "2px",
+        opacity: 1
+      });
+      clicked = false;
+    }
+  });
 });
+
+  //      $("#cop").animate({
+
+        });
+  //      $("#robber").animate({
+
+        });
