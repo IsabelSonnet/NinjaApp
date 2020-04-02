48a3c1f01a4fc6ce880b5d72fbfd154e7eb24b6b
Samanthacgorton <samantha@Samanthas-MacBook-Pro.local>
2020-04-02 16:41:32 -0700

    changes

--- na_script.js
+++ na_script.js
@@ -1,21 +1,5 @@
 
-//$(document).ready(() => {
-//
-//    $("button").click(function () {
-//        $("#cop").animate({
-//
-//$(document).ready(() => {
-//
-//    $("button").click(function () {
-//        $("#cop").animate({
-//
-//        });
-//        $("#robber").animate({
-//
-//        });
-//    });
-//});
-<<<<<<< HEAD
+$(document).ready(() => {
 $(document).keydown(function (event) {
     var keycode = (event.keyCode ? event.keyCode : event.which);
     if (keycode == '39') {
@@ -33,39 +17,5 @@ $(document).keydown(function (event) {
         });
     }
 });
-//        $(document).keydown(function (event) {
-//    var keycode = (event.keyCode ? event.keyCode : event.which);
-//    if (keycode == '37') {
-//        $("#cop").animate({
-//            right: '+=10px',
-//        });
-//    }
-//=======
-//$(document).ready(() => {
-//
-//$(document).keydown(function(event){
-//     var keycode = (event.keyCode ? event.keyCode : event.which);
-//                if(keycode == '39'){
-//                    $("#copimg").animate({
-//                         left: '+=10px',
-//                    });
-//                }else if(keycode == '100'){
-//                    $("#robberimg").animate({
-//                         left: '+=10px',
-//                    });
-//});
-//
-//>>>>>>> origin/master
-//});
-=======
 
-$(document).ready(() => {
-
-    $(document).keydown(function (event) {
-        var keycode = (event.keyCode ? event.keyCode : event.which);
-        if (keycode == '39') {
-            $("#copimg").animate({
-                left: '+=10px',
-            });
-    }
->>>>>>> origin/master
+});
