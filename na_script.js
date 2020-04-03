08c3bde1b9ebb0e43ed79b8518c937e0232dbc9c
Samanthacgorton <samantha@Samanthas-MacBook-Pro.local>
2020-04-02 17:41:28 -0700

    New pengin

--- na_script.js
+++ na_script.js
@@ -1,36 +1,77 @@
 $(document).ready(() => {
 
-$(document).keydown(function (event) {
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-    if (keycode == '39') {
-        $("#ninja").animate({
-            left: '+=20px',
-        });
-    }
+    $(document).keydown(function (event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode == '39') {
+            $("#ninja").animate({
+                left: '+=20px',
+            });
+        }
+    });
+
+    $(document).keydown(function (event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode == '37') {
+            $("#ninja").animate({
+                left: '-=20px',
+            });
+        }
+    });
+
+    $(document).keydown(function (event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode == '40') {
+            $("#ninja").animate({
+                top: '+=20px',
+            });
+        }
+    });
+
+    $(document).keydown(function (event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode == '38') {
+            $("#ninja").animate({
+                top: '-=20px',
+            });
+        }
+    });
 });
 
-$(document).keydown(function (event) {
-    if (keycode == '37') {
-        $("#ninja").animate({
-            left: '-=20px',
-        });
-    }
+$(document).ready(() => {
+    $(document).keydown(function (event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode == '68') {
+            $("#cop").animate({
+                left: '+=20px',
+            });
+        }
+    });
 
-});$(document).keydown(function (event) {
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-    if (keycode == '40') {
-        $("#ninja").animate({
-            top: '+=20px',
-        });
-    }
+    $(document).keydown(function (event) {
+        if (keycode == '65') {
+            $("#cop").animate({
+                left: '-=20px',
+            });
+        }
+    });
 
-});$(document).keydown(function (event) {
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-    if (keycode == '38') {
-        $("#ninja").animate({
-            top: '-=20px',
-        });
-    }
-});
+    $(document).keydown(function (event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode == '87') {
+            $("#cop").animate({
+                top: '+=20px',
+            });
+        }
+    });
+
+    $(document).keydown(function (event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode == '83') {
+            $("#cop").animate({
+                top: '-=20px',
+            });
+        }
+    });
+
 
 });
