28e2326ccfe1c52e3cb4d7252a969bfdb5b6e1dc
Samanthacgorton <samantha@Samanthas-MacBook-Pro.local>
2020-04-02 17:16:33 -0700

    WORKSSSSS!!!!!!!

--- na_script.js
+++ na_script.js
@@ -4,7 +4,7 @@ $(document).keydown(function (event) {
     var keycode = (event.keyCode ? event.keyCode : event.which);
     if (keycode == '39') {
         $("#ninja").animate({
-            left: '+=10px',
+            left: '+=20px',
         });
     }
 });
@@ -13,7 +13,23 @@ $(document).keydown(function (event) {
     var keycode = (event.keyCode ? event.keyCode : event.which);
     if (keycode == '37') {
         $("#ninja").animate({
-            right: '+=10px',
+            left: '-=20px',
+        });
+    }
+
+});$(document).keydown(function (event) {
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if (keycode == '40') {
+        $("#ninja").animate({
+            top: '+=20px',
+        });
+    }
+
+});$(document).keydown(function (event) {
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if (keycode == '38') {
+        $("#ninja").animate({
+            top: '-=20px',
         });
     }
 });
